e3cd128713e564a22acae14f0f015464c55286a4
Richard Gleeson <gleesonj@hotmail.com>
2018-04-20 12:00:12 +1000

    commit for Jacob

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'should have matching email' do
+    user = User.new
+    user.email = ENV.fetch('USER_EMAIL')
+    expect(user.email).to eq('richard@example.com')
+  end
 end
